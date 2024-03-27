05ee600ec54a3839a5d47b1d222886a9090202cd
akkusorhan <110795488+akkusorhan@users.noreply.github.com>
2024-03-27 16:15:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -55,68 +55,15 @@ app.post("/newcontact", (req, res) => {
     const newEntry = {
         parent: { "type": "database_id", "database_id": process.env.NOTION_DATABASE_ID },
 
-        properties: {
-            "First Name": {
-                "rich_text": [
-                    {
-                        "type": "text",
-                        "text": {
-                            "content": req.body.data.firstName
-                        }
-                    }
-                ]
-            },
-            "Last Name": {
-                "rich_text": [
-                    {
-                        "type": "text",
-                        "text": {
-                            "content": req.body.data.lastName
-                        }
-                    }
-                ]
-            },
-            "Email": {
-                "email": req.body.data.email
-            },
-            "Phone": {
-                "phone_number": req.body.data.phone
-            },
-            "Service": {
-                "rich_text": [
-                    {
-                        "type": "text",
-                        "text": {
-                            "content": req.body.data.service
-                        }
-                    }
-                ]
-            },
-            "Location": {
-                "rich_text": [
-                    {
-                        "type": "text",
-                        "text": {
-                            "content": req.body.data.location
-                        }
-                    }
-                ]
-            },
-            "Lead Source": {
-                "rich_text": [
-                    {
-                        "type": "text",
-                        "text": {
-                            "content": req.body.data.source
-                        }
-                    }
-                ]
-            },
-            "Status": {
-                "status": {
-                    "name": "Lead Generated"
-                }
-            },
+        properties: { 
+            "First Name": { "rich_text": [{ "type": "text", "text": { "content": req.body.data.firstName } }] },
+            "Last Name": { "rich_text": [{ "type": "text", "text": { "content": req.body.data.lastName } }] },
+            "Email": { "email": req.body.data.email },
+            "Phone": { "phone_number": req.body.data.phone },
+            "Service": { "rich_text": [{ "type": "text", "text": { "content": req.body.data.service } }] },
+            "Location": { "rich_text": [{ "type": "text", "text": { "content": req.body.data.location } }] },
+            "Lead Source": { "rich_text": [{ "type": "text", "text": { "content": req.body.data.source } }] },
+            "Status": { "status": { "name": "Lead Generated" } },
 
         }
     }
