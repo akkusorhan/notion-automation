5f0a707717e982d1c2a5a54f7755afdc42430574
akkusorhan <110795488+akkusorhan@users.noreply.github.com>
2024-03-30 13:07:40 -0700

    updates

--- GmailLogic.js
+++ GmailLogic.js
@@ -1,40 +1,11 @@
 const nodemailer = require("nodemailer")
 const schedule = require("node-schedule")
+const { google } = require("googleapis")
+const { JWT } = require('google-auth-library');
 
-const EMAIL_SERVICE = "gmail"
-const EMAIL_USER = process.env.EMAIL_USER
-const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
-const RECIPIENT_EMAIL = process.env.RECIPIENT_EMAIL
-
-const transponder = nodemailer.createTransport({
-    service: EMAIL_SERVICE,
-    auth: {
-        user: EMAIL_USER,
-        pass: EMAIL_PASSWORD
-    }
-})
-
-function sendEmail(subject, body) {
-    const mailOptions = {
-        from: EMAIL_USER,
-        to: RECIPIENT_EMAIL,
-        subject: subject,
-        body: body
-    }
-
-    transponder.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log("Error sending email: ", error)
-        } else {
-            console.log("Email sent successfully: ", info.response)
-        }
-    })
-}
 
 function GmailLogic(recipientEmail) {
-    const emailSubject = "Test Subject"
-    const emailBody = "Test Body"
-    sendEmail(emailSubject, emailBody)
+
 }
 
 module.exports = GmailLogic
--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const { Client } = require("@notionhq/client")
 const GmailLogic = require("./GmailLogic")
 require("dotenv").config()
 
+const nodemailer = require("nodemailer")
+
+
+const { google } = require("googleapis")
+const { JWT } = require('google-auth-library');
+const bodyParser = require('body-parser');
+
 const app = express()
 const notion = new Client({ auth: process.env.NOTION_API_KEY })
 
@@ -12,6 +19,82 @@ let port = process.env.PORT || 3000
 
 app.listen(port)
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Service account credentials
+const SERVICE_ACCOUNT_EMAIL = process.env.SERVICE_ACCOUNT_EMAIL // Service account email address
+const PRIVATE_KEY = process.env.PRIVATE_KEY // Service account private key (replace newlines with '\n')
+
+// OAuth2 client using service account credentials
+const jwtClient = new JWT({
+    email: SERVICE_ACCOUNT_EMAIL,
+    key: PRIVATE_KEY,
+    scopes: ['https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/gmail.readonly']
+})
+
+// OAuth2 authentication
+async function authenticate() {
+    try {
+        await jwtClient.authorize();
+        console.log("Authentication successful") //jwtClient.credentials
+        // Log access token
+        // console.log("Access Token:", jwtClient.credentials.access_token);
+        return jwtClient.credentials.access_token;
+
+    } catch (error) {
+        console.log("Error occurred during authentication: ", error)
+    }
+}
+
+// Initialize authentication
+// authenticate()
+
+// Send email function
+async function sendEmail() {
+    try {
+        console.log("Access Token:", jwtClient.credentials.access_token); // Log access token
+        // create Nodemailer transporter using JWT client for authentication
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            host: 'smtp-relay.gmail.com', // Gmail SMTP relay server
+            port: 465, // SMTP port (Gmail uses port 465 for SSL/TLS)
+            secure: true, // Use SSL/TLS
+            auth: {
+                type: "OAuth2",
+                user: process.env.SERVICE_ACCOUNT_EMAIL,
+                accessToken: jwtClient.credentials.access_token // jwtClient.credentials.access_token
+            }
+        })
+
+        // Define email options
+        const mailOptions = {
+            from: process.env.SERVICE_ACCOUNT_EMAIL, 
+            to: process.env.RECIPIENT_EMAIL,
+            subject: "Test Email",
+            text: "This is a test email"
+        }
+
+        // Send email
+        const info = await transporter.sendMail(mailOptions)
+        console.log("Email send successfully: ", info)
+    } catch (error) {
+        console.error("Error occurred while sending email: ", error)
+    }
+}
+
+async function emailAutomation() {
+    try {
+        await authenticate()
+        await sendEmail()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+emailAutomation()
+
+// sendEmail()
 
 mongoose.connect("mongodb+srv://admin:" + process.env.DB_PASSWORD + process.env.MONGODB_CLUSTER)
 
@@ -85,6 +168,7 @@ app.post("/newcontact", (req, res) => {
 app.get("/newcontact", (req, res) => {
     const randomText = "text"
     GmailLogic(randomText)
+
     Contact.find()
     .select("data firstName lastName email phone service location source")
     .exec()
