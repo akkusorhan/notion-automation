a3ef53842363dbb78303d14cfbd5c50f4c3f73b8
akkusorhan <akkusorhan2@gmail.com>
2024-03-31 00:14:12 -0700

    updates

--- app.js
+++ app.js
@@ -22,54 +22,21 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Service account credentials
-const SERVICE_ACCOUNT_EMAIL = process.env.SERVICE_ACCOUNT_EMAIL // Service account email address
-const PRIVATE_KEY = process.env.PRIVATE_KEY // Service account private key (replace newlines with '\n')
-
-// OAuth2 client using service account credentials
-const jwtClient = new JWT({
-    email: SERVICE_ACCOUNT_EMAIL,
-    key: PRIVATE_KEY,
-    scopes: ['https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/gmail.readonly']
-})
-
-// OAuth2 authentication
-async function authenticate() {
-    try {
-        await jwtClient.authorize();
-        console.log("Authentication successful") //jwtClient.credentials
-        // Log access token
-        // console.log("Access Token:", jwtClient.credentials.access_token);
-        return jwtClient.credentials.access_token;
-
-    } catch (error) {
-        console.log("Error occurred during authentication: ", error)
-    }
-}
-
-// Initialize authentication
-// authenticate()
-
 // Send email function
 async function sendEmail() {
     try {
-        console.log("Access Token:", jwtClient.credentials.access_token); // Log access token
-        // create Nodemailer transporter using JWT client for authentication
         const transporter = nodemailer.createTransport({
             service: "gmail",
-            host: 'smtp-relay.gmail.com', // Gmail SMTP relay server
-            port: 465, // SMTP port (Gmail uses port 465 for SSL/TLS)
-            secure: true, // Use SSL/TLS
             auth: {
-                type: "OAuth2",
-                user: process.env.SERVICE_ACCOUNT_EMAIL,
-                accessToken: jwtClient.credentials.access_token // jwtClient.credentials.access_token
-            }
+                user: process.env.SENDER_EMAIL, // process.env.SERVICE_ACCOUNT_EMAIL,
+                pass: process.env.SMTP_APP_PASSWORD
+            },
+            authMethod: "PLAIN"
         })
 
         // Define email options
         const mailOptions = {
-            from: process.env.SERVICE_ACCOUNT_EMAIL, 
+            from: process.env.SENDER_EMAIL, 
             to: process.env.RECIPIENT_EMAIL,
             subject: "Test Email",
             text: "This is a test email"
@@ -77,7 +44,7 @@ async function sendEmail() {
 
         // Send email
         const info = await transporter.sendMail(mailOptions)
-        console.log("Email send successfully: ", info)
+        console.log("Email sent successfully: ", info)
     } catch (error) {
         console.error("Error occurred while sending email: ", error)
     }
@@ -85,16 +52,12 @@ async function sendEmail() {
 
 async function emailAutomation() {
     try {
-        await authenticate()
         await sendEmail()
     } catch (error) {
         console.log(error)
     }
 }
-
 emailAutomation()
-
-// sendEmail()
 
 mongoose.connect("mongodb+srv://admin:" + process.env.DB_PASSWORD + process.env.MONGODB_CLUSTER)
 
